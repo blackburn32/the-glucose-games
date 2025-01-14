ade8e9955af4520256b16380b670c7d49f816930
blackburn32 <alexblackburn32@gmail.com>
2025-01-13 16:35:54 -0800

    Update log level

--- server/utils/database/deleteToken.ts
+++ server/utils/database/deleteToken.ts
@@ -4,10 +4,10 @@ import type { Database } from '~/types/database.types.ts'
 export const deleteToken = async (user_id: string, provider: string, client: SupabaseClient<Database>) => {
   const { error } = await client.from('oauth_tokens').delete().eq('user_id', user_id).eq('provider', provider)
   if (error) {
-    console.log('Failed to delete token', error)
-    // throw createError({
-    //   statusCode: 500,
-    //   statusMessage: 'Failed to delete token',
-    // })
+    console.error('Failed to delete token', error)
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Failed to delete token',
+    })
   }
 }
--- server/utils/database/storeToken.ts
+++ server/utils/database/storeToken.ts
@@ -5,7 +5,7 @@ import { deleteToken } from '~/server/utils/database/deleteToken.ts'
 export const storeToken = async (tokenData: Database['public']['Tables']['oauth_tokens']['Insert'], client: SupabaseClient<Database>) => {
   const { data, error } = await client.from('oauth_tokens').insert(tokenData)
   if (error) {
-    console.log('Failed to store token', error)
+    console.error('Failed to store token', error)
     throw createError({
       statusCode: 500,
       statusMessage: 'Failed to store token',
