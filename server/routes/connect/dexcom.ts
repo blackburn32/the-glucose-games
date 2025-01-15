3d09a0c4cbc2dae145634ca5bad6d322d8505f7c
blackburn32 <alexblackburn32@gmail.com>
2025-01-15 13:58:33 -0800

    Use proper runtime config

--- server/routes/connect/dexcom.ts
+++ server/routes/connect/dexcom.ts
@@ -1,11 +1,7 @@
 import { serverSupabaseUser } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-  const runtimeConfig = useRuntimeConfig()
-  const supabaseServiceKey = runtimeConfig.supabase.serviceKey
-  const supabaseApiKey = runtimeConfig.public.supabase.key
-  console.error('supabaseServiceKey', supabaseServiceKey)
-  console.error('supabase api key', supabaseApiKey)
+  const runtimeConfig = useRuntimeConfig(event)
   const user = await serverSupabaseUser(event)
 
   if (!user) {
