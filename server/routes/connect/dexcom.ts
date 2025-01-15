d7b9d9e2cd2bc2a6362cf43af94ff5e6e76905c3
blackburn32 <alexblackburn32@gmail.com>
2025-01-15 11:34:31 -0800

    Move logs

--- server/routes/connect/dexcom.ts
+++ server/routes/connect/dexcom.ts
@@ -1,6 +1,10 @@
 import { serverSupabaseUser } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
+  const supabaseServiceKey = runtimeConfig.supabase.serviceKey
+  const supabaseApiKey = runtimeConfig.public.supabase.key
+  console.error('supabaseServiceKey', supabaseServiceKey)
+  console.error('supabase api key', supabaseApiKey)
   const user = await serverSupabaseUser(event)
   const runtimeConfig = useRuntimeConfig(event)
 
@@ -14,11 +18,6 @@ export default defineEventHandler(async (event) => {
   const dexcomBaseUrl = runtimeConfig.dexcomBaseUrl
   const dexcomClientId = runtimeConfig.dexcomClientId
   const redirectUrl = runtimeConfig.dexcomRedirectUrl
-
-  const supabaseServiceKey = runtimeConfig.supabase.serviceKey
-  const supabaseApiKey = runtimeConfig.public.supabase.key
-  console.error('supabaseServiceKey', supabaseServiceKey)
-  console.error('supabase api key', supabaseApiKey)
 
   const url = `${dexcomBaseUrl}/v2/oauth2/login?client_id=${dexcomClientId}&redirect_uri=${redirectUrl}&response_type=code&scope=offline_access&state=${user.id}`
 
