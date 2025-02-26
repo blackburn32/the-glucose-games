a6374774a931fb279f22c6551908cac906d09712
blackburn32 <alexblackburn32@gmail.com>
2025-02-26 12:05:22 -0800

    Try to get Sentry working on server side

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,25 +1,10 @@
-import * as Sentry from "@sentry/nuxt";
+import * as Sentry from '@sentry/nuxt'
 
 Sentry.init({
-  // If set up, you can use your runtime config here
-  // dsn: useRuntimeConfig().public.sentry.dsn,
-  dsn: "https://550b0b2aff9c81090aa5cf68cd787d3f@o4508887036526592.ingest.us.sentry.io/4508887038033920",
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
+  dsn: 'https://550b0b2aff9c81090aa5cf68cd787d3f@o4508887036526592.ingest.us.sentry.io/4508887038033920',
   tracesSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
-  
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
   replaysOnErrorSampleRate: 1.0,
-  
-  // If you don't want to use Session Replay, just remove the line below:
   integrations: [Sentry.replayIntegration()],
-  
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+})
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from "vite";
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { defineConfig } from 'vite'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 export default defineConfig({
   build: {
-    sourcemap: true, // Source map generation must be turned on
+    sourcemap: true,
   },
   plugins: [// Put the Sentry vite plugin after all other plugins
-  sentryVitePlugin({
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-    org: "the-glucose-games",
-    project: "javascript-nuxt",
-  }), sentryVitePlugin({
-    org: "the-glucose-games",
-    project: "javascript-nuxt"
-  })],
-});
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: 'the-glucose-games',
+      project: 'javascript-nuxt',
+    }), sentryVitePlugin({
+      org: 'the-glucose-games',
+      project: 'javascript-nuxt',
+    })],
+})
