cf2d9060830e4c7f8e90175a18b863506e99a34b
blackburn32 <alexblackburn32@gmail.com>
2025-03-05 10:11:40 -0800

    Sentry config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,12 +1,7 @@
-import * as Sentry from "@sentry/nuxt";
- 
-Sentry.init({
-  dsn: "https://550b0b2aff9c81090aa5cf68cd787d3f@o4508887036526592.ingest.us.sentry.io/4508887038033920",
+import * as Sentry from '@sentry/nuxt'
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
+Sentry.init({
+  dsn: 'https://550b0b2aff9c81090aa5cf68cd787d3f@o4508887036526592.ingest.us.sentry.io/4508887038033920',
   tracesSampleRate: 1.0,
-  
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+})
