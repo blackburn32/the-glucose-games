5fad0dcec927d1842fa512f5ac0c25a6af38e1ac
blackburn32 <alexblackburn32@gmail.com>
2025-03-11 16:42:32 -0700

    Delete unnecessary sentry config

--- functions/_middleware.js
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/cloudflare'
-
-export const onRequest = [
-  // Make sure Sentry is the first middleware
-  Sentry.sentryPagesPlugin(() => ({
-    dsn: 'https://550b0b2aff9c81090aa5cf68cd787d3f@o4508887036526592.ingest.us.sentry.io/4508887038033920',
-    tracesSampleRate: 1.0,
-  })),
-]
