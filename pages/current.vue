58e065404fe6c15b20021cb7fe38e47eedb53040
blackburn32 <alexblackburn32@gmail.com>
2025-03-19 09:26:01 -0700

    Add loading indicator on current game screen

--- pages/current.vue
+++ pages/current.vue
@@ -30,7 +30,7 @@
         </div>
       </NuxtLink>
     </div>
-    <div v-if="!hasGlucoseData">
+    <div v-if="!hasGlucoseData && !glucoseDataLoading">
       <NuxtLink
         class="btn btn-outline"
         to="/connect"
@@ -42,10 +42,18 @@
       v-if="hasGlucoseData"
       :thresholds="thresholds"
     />
+    <div
+      v-if="glucoseDataLoading"
+      class="flex flex-col w-full items-center space-y-4 px-2"
+    >
+      <div class="text-4xl font-bold mt-10">
+        Loading...
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const { thresholds } = useThresholds()
-const { hasGlucoseData } = useGlucoseValues()
+const { hasGlucoseData, glucoseDataLoading } = useGlucoseValues()
 </script>
