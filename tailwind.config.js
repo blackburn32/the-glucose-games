b09bea9e6a55a4032a6dd049b1320bc60581b307
blackburn32 <alexblackburn32@gmail.com>
2025-03-20 11:07:45 -0700

    Remove daisy UI for upgrade process

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import daisyui from 'daisyui'
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
@@ -48,26 +46,5 @@ export default {
       },
     },
   },
-  plugins: [
-    daisyui,
-  ],
 
-  daisyui: {
-    logs: false,
-    themes: [{
-      dark: {
-        'primary': '#00c951',
-        'secondary': '#E26216',
-        'accent': '#EBE5E1',
-        'neutral': '#EBE5E1',
-        'base-100': '#111318',
-        'info': '#00ff66',
-        'success': '#00ff66',
-        'warning': '#ed8345',
-        'error': '#ff5449',
-        '--rounded-box': '0.25rem',
-        '--rounded-btn': '0.25rem',
-      },
-    }],
-  },
 }
