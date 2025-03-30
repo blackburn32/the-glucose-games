838e686437199d535dc4fbeddea728c854d33071
blackburn32 <alexblackburn32@gmail.com>
2025-03-30 11:13:59 -0700

    Stop rounding average bg

--- utils/scoring/averageGlucoseValue/averageGlucoseValue.ts
+++ utils/scoring/averageGlucoseValue/averageGlucoseValue.ts
@@ -5,5 +5,5 @@ export const scoreRecordsByAverageGlucose = (records: GlucoseRecord[]) => {
   const sum = records.reduce((acc, record) => {
     return acc + record.value
   }, 0)
-  return Math.round(sum / total)
+  return sum / total
 }
