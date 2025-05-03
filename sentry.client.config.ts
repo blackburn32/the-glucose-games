a3569491119e94283c0a69e696b179bd912e289b
blackburn32 <alexblackburn32@gmail.com>
2025-05-03 09:13:13 -0700

    Turn on profiling

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,11 +1,12 @@
 import * as Sentry from '@sentry/nuxt'
 
 Sentry.init({
-  dsn: 'https://550b0b2aff9c81090aa5cf68cd787d3f@o4508887036526592.ingest.us.sentry.io/4508887038033920',
+  dsn: process.env.SENTRY_DSN,
   tracesSampleRate: 1.0,
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
   integrations: [Sentry.replayIntegration()],
   debug: false,
   environment: process.env.SENTRY_ENVIRONMENT,
+  profilesSampleRate: 1.0,
 })
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,11 +1,15 @@
 import * as Sentry from '@sentry/nuxt'
 import dotenv from 'dotenv'
+import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 dotenv.config()
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
   debug: false,
   environment: process.env.SENTRY_ENVIRONMENT,
+  profileSessionSampleRate: 1.0,
+  profileLifecycle: 'trace',
 })
