803b61224e456c5e2290bd3c325c3a2cd40c1831
blackburn32 <alexblackburn32@gmail.com>
2025-05-03 09:29:09 -0700

    Turn off server profiling

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,15 +1,11 @@
 import * as Sentry from '@sentry/nuxt'
 import dotenv from 'dotenv'
-import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
 dotenv.config()
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
-  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
   debug: false,
   environment: process.env.SENTRY_ENVIRONMENT,
-  profileSessionSampleRate: 1.0,
-  profileLifecycle: 'trace',
 })
