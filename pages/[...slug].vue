d81edb4d003ab1e36a690f986455de4ff89ee9ab
blackburn32 <alexblackburn32@gmail.com>
2025-06-18 12:32:37 -0700

    feat: add catch-all route for content pages with dashboard redirect

---
+++ pages/[...slug].vue
@@ -0,0 +1,18 @@
+<template>
+  <DashboardPanelWithHeader
+    id="current"
+    title="Current Games"
+  >
+    <div class="flex flex-col w-full items-center space-y-4 px-2">
+      <div class="text-4xl font-bold mt-10">
+        Loading...
+      </div>
+    </div>
+  </DashboardPanelWithHeader>
+</template>
+
+<script setup lang="ts">
+onMounted(() => {
+  navigateTo('/dashboard')
+})
+</script>
